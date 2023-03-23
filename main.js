f27fcf825699d765a533330b396f6e7eb40bd336
Juan Pablo Pieroni <seisduro@gmail.com>
2023-03-23 11:11:34 -0300

    html y agregar carrito

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+let carrito = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')){
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        actualizarCarrito()
+    }
+})
+
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'info',
+        title: 'El carrito está vacio'
+      })
+    actualizarCarrito()
+    })
+
+    //Se agrega producot del stock.json.
