647e279a335be412c5d466646ee1e280cf794dee
Juan Pablo Pieroni <seisduro@gmail.com>
2023-03-23 11:23:41 -0300

    dar estilo y bug carrito y json

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
+const contenedorProductos = document.getElementById('contenedor-productos')
 
+const contenedorFetch = document.getElementById('contenedor-fetch')
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const botonVaciar = document.getElementById('vaciar-carrito')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
+
+const cantidad = document.getElementById('cantidad')
+const precioTotal = document.getElementById('precioTotal')
+const cantidadTotal = document.getElementById('cantidadTotal')
 
 let carrito = [];
 
@@ -31,3 +43,42 @@ botonVaciar.addEventListener('click', () => {
     })
 
     //Se agrega producot del stock.json.
+
+    stockProductos.forEach((producto) => {
+        const div = document.createElement('div')
+        div.classList.add('producto')
+        div.innerHTML = `
+        <img src=${producto.img} alt= "">
+        <h3>${producto.nombre}</h3>
+        <p>${producto.desc}</p>
+        <p>piel: ${producto.piel}</p>
+        <p class="precioProducto">Precio:$ ${producto.precio}</p>
+        <button id="agregar${producto.id}" class="boton-agregar">Comprar Ya <i class="fas fa-shopping-cart"></i></button>
+    
+        `
+        contenedorProductos.appendChild(div)
+    
+        const boton = document.getElementById(`agregar${producto.id}`)
+    
+        boton.addEventListener('click', () => {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+              })
+              
+              Toast.fire({
+                icon: 'success',
+                title: '¡Su producto se agregó con éxito!'
+              })
+            agregarAlCarrito(producto.id)
+        })
+    })
+
+    
