febd4bd28ba1f302fea1008404989c20b8c2cf3d
Juan Pablo Pieroni <seisduro@gmail.com>
2023-03-24 08:41:02 -0300

    check list

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 const contenedorProductos = document.getElementById('contenedor-productos')
-
 const contenedorFetch = document.getElementById('contenedor-fetch')
-
 const contenedorCarrito = document.getElementById('carrito-contenedor')
-
 const botonVaciar = document.getElementById('vaciar-carrito')
-
 const contadorCarrito = document.getElementById('contadorCarrito')
-
 const cantidad = document.getElementById('cantidad')
 const precioTotal = document.getElementById('precioTotal')
 const cantidadTotal = document.getElementById('cantidadTotal')
@@ -15,70 +10,146 @@ const cantidadTotal = document.getElementById('cantidadTotal')
 let carrito = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('carrito')){
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-        actualizarCarrito()
-    }
+  if (localStorage.getItem('carrito')) {
+    carrito = JSON.parse(localStorage.getItem('carrito'))
+    actualizarCarrito()
+  }
 })
 
 botonVaciar.addEventListener('click', () => {
-    carrito.length = 0
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-      
-      Toast.fire({
-        icon: 'info',
-        title: 'El carrito está vacio'
-      })
-    actualizarCarrito()
-    })
+  carrito.length = 0
+  const Toast = Swal.mixin({
+    toast: true,
+    position: 'top',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
 
-    //Se agrega producot del stock.json.
+  Toast.fire({
+    icon: 'info',
+    title: 'El carrito está vacio'
+  })
+  actualizarCarrito()
+})
 
-    stockProductos.forEach((producto) => {
-        const div = document.createElement('div')
-        div.classList.add('producto')
-        div.innerHTML = `
+//Se agrega producot del stock.json.
+
+stockProductos.forEach((producto) => {
+  const div = document.createElement('div')
+  div.classList.add('producto')
+  div.innerHTML = `
         <img src=${producto.img} alt= "">
         <h3>${producto.nombre}</h3>
         <p>${producto.desc}</p>
-        <p>piel: ${producto.piel}</p>
+        <p>
+        Piel: ${producto.piel}</p>
         <p class="precioProducto">Precio:$ ${producto.precio}</p>
-        <button id="agregar${producto.id}" class="boton-agregar">Comprar Ya <i class="fas fa-shopping-cart"></i></button>
+        <button id="agregar${producto.id}" class="boton-agregar">Agregar al carrito<i class="fas fa-shopping-cart"></i></button>
     
         `
-        contenedorProductos.appendChild(div)
-    
-        const boton = document.getElementById(`agregar${producto.id}`)
-    
-        boton.addEventListener('click', () => {
-            const Toast = Swal.mixin({
-                toast: true,
-                position: 'top',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                  toast.addEventListener('mouseenter', Swal.stopTimer)
-                  toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
-              })
-              
-              Toast.fire({
-                icon: 'success',
-                title: '¡Su producto se agregó con éxito!'
-              })
-            agregarAlCarrito(producto.id)
-        })
-    })
+  contenedorProductos.appendChild(div)
 
-    
+  const boton = document.getElementById(`agregar${producto.id}`)
+
+  boton.addEventListener('click', () => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    Toast.fire({
+      icon: 'success',
+      title: '¡Su producto se agregó con éxito!'
+    })
+    agregarAlCarrito(producto.id)
+  })
+})
+
+/* se agrega producto del Json */
+
+fetch('/stock.json')
+  .then(resp => resp.json())
+  .then(stock => {
+    stock.forEach(producto => {
+      const div = document.createElement('div')
+      div.classList.add('productoFetch')
+      div.innerHTML = `
+                    <img src= ${producto.img} alt="">
+                    <h3>${producto.nombre}</h3>
+                    <p>${producto.desc}</p>
+                    <p>Piel:${producto.piel}</p>
+                    <p class="precioProducto">Precio:$ ${producto.precio}</p>
+                    <button id="agregar${producto.id}" class="boton-agregar">Proximamente <i class="fas fa-shopping-cart"></i></button>
+            `
+
+      contenedorFetch.appendChild(div)
+
+
+    })
+  });
+
+/* Fun agregar carrito */
+
+
+const agregarAlCarrito = (prodId) => {
+  const existe = carrito.some(prod => prod.id === prodId)
+
+  if (existe) {
+    const prod = carrito.map(prod => {
+      if (prod.id === prodId) {
+        prod.cantidad++
+      }
+    })
+  } else {
+    const item = stockProductos.find((prod) => prod.id === prodId)
+    carrito.push(item)
+  }
+
+  actualizarCarrito()
+}
+
+const eliminarDelCarrito = (prodId) => {
+  const productoIndex = carrito.findIndex((prod) => prod.id == prodId)
+  carrito.splice(productoIndex, 1)
+  actualizarCarrito()
+
+  console.log(carrito)
+}
+
+const actualizarCarrito = () => {
+  contenedorCarrito.innerHTML = ""
+  carrito.forEach((prod) => {
+    const div = document.createElement('div')
+    div.className = ('productoEnCarrito')
+    div.innerHTML = `
+      <p>${prod.nombre}</p>
+      <p>Precio:$${prod.precio}</p>
+      <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+      <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
+      `
+
+    contenedorCarrito.appendChild(div)
+
+
+
+  })
+  contadorCarrito.innerText = carrito.length
+  console.log(carrito)
+  precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
+  guardarCarrito()
+}
+
+const guardarCarrito = () => {
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+}
